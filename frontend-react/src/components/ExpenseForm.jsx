db41cc11ba79d055aea9689602226e11427f6e19
AkshayChavan <akshay11cb@gmail.com>
2024-07-16 13:40:06 +0530

    add deployed backend url

--- frontend-react/src/components/ExpenseForm.jsx
+++ frontend-react/src/components/ExpenseForm.jsx
@@ -1,58 +1,61 @@
-import Axios from 'axios'; // Import axios
-import React, { useState } from 'react';
-import './css/expense-form.css'; // In ExpenseForm.js
-
+import Axios from "axios"; // Import axios
+import React, { useState } from "react";
+import "./css/expense-form.css"; // In ExpenseForm.js
+const url = require("../config");
 
 function ExpenseForm({ user_id }) {
-    const [description, setDescription] = useState('');
-    const [amount, setAmount] = useState(0);
-    const [category, setCategory] = useState('')
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState("");
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        await Axios.post(`http://localhost:8080/api/expenses?userID=${user_id}`, {
-            description,
-            amount,
-            category
-        }).then((response) => {
-            setDescription('');
-            setAmount(0);
-        }).catch((err) => {
-            console.log(err)
-        });
+    await Axios.post(`${url.BACKEND_URL}/api/expenses?userID=${user_id}`, {
+      description,
+      amount,
+      category,
+    })
+      .then((response) => {
+        setDescription("");
+        setAmount(0);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    };
-
-    return (
-        <div className="expense-form">
-            <h1>Add Expense</h1>
-            <form>
-                <label htmlFor="description">Description:</label>
-                <input
-                    type="text"
-                    id="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                />
-                <label htmlFor="amount">Amount:</label>
-                <input
-                    type="number"
-                    id="amount"
-                    value={amount}
-                    onChange={(e) => setAmount(e.target.value)}
-                />
-                <label htmlFor="category">Category:</label>
-                <input
-                    type="category"
-                    id="category"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                />
-                <button onClick={handleSubmit} type="submit">Submit</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className="expense-form">
+      <h1>Add Expense</h1>
+      <form>
+        <label htmlFor="description">Description:</label>
+        <input
+          type="text"
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <label htmlFor="amount">Amount:</label>
+        <input
+          type="number"
+          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <label htmlFor="category">Category:</label>
+        <input
+          type="category"
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        <button onClick={handleSubmit} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default ExpenseForm;
--- frontend-react/src/components/ExpenseList.jsx
+++ frontend-react/src/components/ExpenseList.jsx
@@ -1,42 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import './css/expense-list.css'; // In ExpenseList.js
-import Axios from 'axios'; // Import axios
+import React, { useState, useEffect } from "react";
+import "./css/expense-list.css";
+import Axios from "axios";
 
 function ExpenseList({ user_id }) {
-    const [expenses, setExpenses] = useState([]);
+  const [expenses, setExpenses] = useState([]);
 
-    useEffect(() => {
-        Axios.get(`http://localhost:8080/api/user/expenses`, { params: { user_id: user_id } }).then((response) => {
-            setExpenses(response.data)
-        }).catch((err) => {
-            console.log(err)
-        });
-    }, [user_id]);
+  useEffect(() => {
+    Axios.get(`http://3.110.175.71:8080/api/user/expenses`, {
+      params: { user_id: user_id },
+    })
+      .then((response) => {
+        console.log("Req sent and git res: " + " " + response);
+        setExpenses(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [user_id, expenses]);
 
-
-    return (
-        <div className="expense-list">
-            <h1>Expense List</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Description</th>
-                        <th>Category</th>
-                        <th>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {expenses.map((expense) => (
-                        <tr key={expense.expenseId}>
-                            <td>{expense.description}</td>
-                            <td>{expense.category}</td>
-                            <td>{expense.amount}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="expense-list">
+      <h1>Expense List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.expenseId}>
+              <td>{expense.description}</td>
+              <td>{expense.category}</td>
+              <td>{expense.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ExpenseList;
